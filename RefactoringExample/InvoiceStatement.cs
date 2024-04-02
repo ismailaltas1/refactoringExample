3b6707d1cbf144ce65f380a2f726e22c344acea1
ismail.altas <ismail.altas@gktech.io>
2024-04-02 11:57:27 +0300

    1.1 rename name

--- RefactoringExample/InvoiceStatement.cs
+++ RefactoringExample/InvoiceStatement.cs
@@ -38,29 +38,29 @@ public class InvoiceStatement
 
     private static decimal getAmount(Play play, Performance perf)
     {
-        decimal thisAmount = 0;
+        decimal result = 0;
         switch (play.Type)
         {
             case "tragedy":
-                thisAmount = 40000;
+                result = 40000;
                 if (perf.Audience > 30)
                 {
-                    thisAmount += 1000 * (perf.Audience - 30);
+                    result += 1000 * (perf.Audience - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.Audience > 20)
                 {
-                    thisAmount += 10000 + 500 * (perf.Audience - 20);
+                    result += 10000 + 500 * (perf.Audience - 20);
                 }
-                thisAmount += 300 * perf.Audience;
+                result += 300 * perf.Audience;
                 break;
             default:
                 throw new Exception($"unknown type: {play.Type}");
         }
 
-        return thisAmount;
+        return result;
     }
 }
 
