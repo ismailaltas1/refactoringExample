219e45ef7be6d6689e1414e4fd99ede26b6ef627
ismailaltas1 <ismailaltas1@gmail.com>
2024-04-06 18:13:07 +0300

    1.16 update unit tests name

--- RefactoringExample.UnitTests/StatementRenderingTests.cs
+++ RefactoringExample.UnitTests/StatementRenderingTests.cs
@@ -6,7 +6,7 @@ using RefactoringExample.Statements;
 
 namespace RefactoringExample.UnitTests;
 
-public class Tests
+public class StatementRenderingUnitTests
 {
     private Invoice invoice;
     private StatementData _statementData;
@@ -17,22 +17,22 @@ public class Tests
         _statementData = new StatementCalculator().CreateStatementData(invoice);
     }
     [Test]
-    public void TestGenerateStatement()
+    public void GenerateStatement_ShouldReturnExpectedPlainTextStatement()
     {
         
         var actualResult =  RenderFactory.Render(_statementData, RenderType.PlainText);
-        var expectedStatement = "Statement for BigCo\n  hamlet: $450.00 (35 seats)\n  as-like: $580.00 (35 seats)\n  othello: $400.00 (20 seats)\n  as-like-2: $354.00 (18 seats)\nAmount owed is $1,784.00\nYou earned 20 credits\n";
+        const string expectedStatement = "Statement for BigCo\n  hamlet: $450.00 (35 seats)\n  as-like: $580.00 (35 seats)\n  othello: $400.00 (20 seats)\n  as-like-2: $354.00 (18 seats)\nAmount owed is $1,784.00\nYou earned 20 credits\n";
         
         actualResult.Should().Be(expectedStatement);
     }
 
 
     [Test]
-    public void TestGenerateHtmlStatement()
+    public void GenerateHtmlStatement_ShouldReturnExpectedHtmlStatement()
     {
         var actualResult =  RenderFactory.Render(_statementData, RenderType.Html);
         
-        string expectedStatement = "<h1>Statement for BigCo</h1>\n<table>\n<tr><th>play</th><th>seats</th><th>cost</th></tr><tr><td>hamlet</td><td>35</td><td>$450.00</td></tr>\n<tr><td>as-like</td><td>35</td><td>$580.00</td></tr>\n<tr><td>othello</td><td>20</td><td>$400.00</td></tr>\n<tr><td>as-like-2</td><td>18</td><td>$354.00</td></tr>\n</table>\n<p>Amount owed is <em>$1,784.00</em></p>\n<p>You earned <em>20</em> credits</p>\n";
+        const string expectedStatement = "<h1>Statement for BigCo</h1>\n<table>\n<tr><th>play</th><th>seats</th><th>cost</th></tr><tr><td>hamlet</td><td>35</td><td>$450.00</td></tr>\n<tr><td>as-like</td><td>35</td><td>$580.00</td></tr>\n<tr><td>othello</td><td>20</td><td>$400.00</td></tr>\n<tr><td>as-like-2</td><td>18</td><td>$354.00</td></tr>\n</table>\n<p>Amount owed is <em>$1,784.00</em></p>\n<p>You earned <em>20</em> credits</p>\n";
 
         actualResult.Should().Be(expectedStatement);
     }
